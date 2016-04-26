6cfb089275cee146be17680d60010a2d4188be3b
nietaki <nietaki@gmail.com>
2016-04-26 16:34:26 +0100

    added unit tests for insert_char()

--- src/stringOps.erl
+++ src/stringOps.erl
@@ -13,9 +13,6 @@
 %% API
 -export([insert_char/3, delete_char/2]).
 
-insert_char(String, Character, Position) ->
-    notok.
-
 delete_char(String, Position) ->
     lists:reverse(delete_character_1(String, Position, [])).
 
@@ -32,4 +29,24 @@ delete_character_test_() ->
         ?_assertEqual("bc", delete_char("abc", 0)),
         ?_assertEqual("ac", delete_char("abc", 1)),
         ?_assertEqual("ab", delete_char("abc", 2))
+    ].
+
+insert_char(String, Character, Position) ->
+    lists:reverse(insert_char_1(String, Character, Position, "")).
+
+%insert_char_1(String, Character, Position, Acc) 
+insert_char_1([], Ch, 0, Acc) -> [Ch|Acc];
+insert_char_1([], _, _, Acc) -> Acc;
+insert_char_1([H|T], Ch, 0, Acc) -> insert_char_1(T, Ch, -1, [H|[Ch|Acc]]);
+insert_char_1([H|T], Ch, X, Acc) -> insert_char_1(T, Ch, X-1, [H|Acc]).
+
+insert_char_test_() ->
+    [
+        ?_assertEqual("x", insert_char("", $x, 0)),
+        ?_assertEqual("", insert_char("", $x, 1)),
+        ?_assertEqual("", insert_char("", $x, -1)),
+        ?_assertEqual("xb", insert_char("b", $x, 0)),
+        ?_assertEqual("ax", insert_char("a", $x, 1)),
+        ?_assertEqual("a", insert_char("a", $x, 2)),
+        ?_assertEqual("axc", insert_char("ac", $x, 1))
     ].
